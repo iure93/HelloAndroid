a741c45ea016743ffef864f393aebae44dac86eb
iure93 <33991424+iure93@users.noreply.github.com>
2017-12-10 23:58:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.helloandroid;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openLink (View view){
+        Uri uri = Uri.parse("http://udacity.com/"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+}
