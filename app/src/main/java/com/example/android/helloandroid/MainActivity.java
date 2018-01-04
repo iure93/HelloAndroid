d84dc3bfc0a0b68a2ccb9830beb61622a7ab953a
iure93 <33991424+iure93@users.noreply.github.com>
2018-01-04 21:47:39 +0100

    Restyle

--- app/src/main/java/com/example/android/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/android/helloandroid/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.android.helloandroid;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +12,5 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
-
-    public void openLink (View view){
-        Uri uri = Uri.parse("http://udacity.com/"); // missing 'http://' will cause crashed
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        startActivity(intent);
-    }
 
 }
